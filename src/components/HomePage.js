b48ed82156a2a0853680cce6fc2bcb2e7e4c22d2
Johanna <adrina0002@gmail.com>
2022-12-20 14:37:42 +0100

    review feedback from lorraine

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,7 +8,7 @@ import IntroLogoLight from "../../static/img/intro_illustration_light.svg";
 
 import AllBlockchains from "../../static/img/all_blockchains.svg";
 //import SolanaLight from "../../static/img/solana_lightmode.svg";
-import SolanaDark from "../../static/img/sol.png";
+import SolanaDark from "../../static/img/solana_dark.svg";
 
 import Avax from "../../static/img/avax.svg";
 import Opt from "../../static/img/opt.svg";
@@ -80,9 +80,7 @@ const listToLoop = [
   },
 ];
 
-let logoStyle = {
-  marginRight: 40,
-};
+let logoStyle = {};
 const supportedBlockchainsList = [
   {
     logo: <Bitcoin style={logoStyle} />,
@@ -121,7 +119,41 @@ const supportedBlockchainsList = [
     title: "AVALANCHE",
   },
   {
-    logo: <img src="./../../static/img/sol.png" width="30" />,
+    logo: (
+      <svg
+        width="30"
+        height="30"
+        viewBox="0 0 30 30"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M15 30C23.2843 30 30 23.2843 30 15C30 6.71573 23.2843 0 15 0C6.71573 0 0 6.71573 0 15C0 23.2843 6.71573 30 15 30Z"
+          fill="black"
+        />
+        <path
+          d="M21.9812 18.0776L19.6596 20.5206C19.6091 20.5737 19.548 20.616 19.4802 20.6449C19.4123 20.6738 19.3391 20.6887 19.2652 20.6887H8.25951C8.207 20.6887 8.15563 20.6736 8.11172 20.6454C8.0678 20.6171 8.03326 20.5769 8.01233 20.5297C7.9914 20.4824 7.98499 20.4302 7.99389 20.3794C8.0028 20.3286 8.02663 20.2815 8.06246 20.2438L10.3858 17.8008C10.4362 17.7479 10.497 17.7056 10.5647 17.6767C10.6323 17.6478 10.7053 17.6328 10.7791 17.6327H21.7841C21.8366 17.6327 21.888 17.6478 21.9319 17.676C21.9758 17.7043 22.0103 17.7445 22.0313 17.7918C22.0522 17.839 22.0586 17.8912 22.0497 17.942C22.0408 17.9928 22.017 18.0399 21.9812 18.0776ZM19.6596 13.158C19.6091 13.1049 19.548 13.0626 19.4802 13.0337C19.4123 13.0048 19.3391 12.9899 19.2652 12.9899H8.25951C8.207 12.9899 8.15563 13.005 8.11172 13.0333C8.0678 13.0615 8.03326 13.1017 8.01233 13.149C7.9914 13.1962 7.98499 13.2485 7.99389 13.2993C8.0028 13.35 8.02663 13.3971 8.06246 13.4348L10.3858 15.8779C10.4362 15.9308 10.497 15.973 10.5647 16.0019C10.6323 16.0308 10.7053 16.0458 10.7791 16.0459H21.7841C21.8366 16.0459 21.888 16.0309 21.9319 16.0026C21.9758 15.9744 22.0103 15.9341 22.0313 15.8869C22.0522 15.8396 22.0586 15.7874 22.0497 15.7366C22.0408 15.6859 22.017 15.6387 21.9812 15.601L19.6596 13.158ZM8.25951 11.4032H19.2652C19.3391 11.4032 19.4123 11.3883 19.4802 11.3594C19.548 11.3305 19.6091 11.2882 19.6596 11.2351L21.9812 8.79205C22.017 8.75438 22.0408 8.70726 22.0497 8.65648C22.0586 8.6057 22.0522 8.55347 22.0313 8.50622C22.0103 8.45896 21.9758 8.41873 21.9319 8.39048C21.888 8.36222 21.8366 8.34717 21.7841 8.34717H10.7791C10.7053 8.34729 10.6323 8.36227 10.5647 8.39118C10.497 8.42009 10.4362 8.46232 10.3858 8.51525L8.06305 10.9583C8.02726 10.9959 8.00344 11.043 7.99452 11.0937C7.98559 11.1444 7.99195 11.1966 8.01281 11.2439C8.03366 11.2911 8.06812 11.3313 8.11194 11.3596C8.15576 11.3879 8.20705 11.403 8.25951 11.4032Z"
+          fill="url(#paint0_linear_1097_1495)"
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear_1097_1495"
+            x1="9.17712"
+            y1="20.9828"
+            x2="20.4737"
+            y2="8.00423"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset="0.08" stop-color="#9945FF" />
+            <stop offset="0.3" stop-color="#8752F3" />
+            <stop offset="0.5" stop-color="#5497D5" />
+            <stop offset="0.6" stop-color="#43B4CA" />
+            <stop offset="0.72" stop-color="#28E0B9" />
+            <stop offset="0.97" stop-color="#19FB9B" />
+          </linearGradient>
+        </defs>
+      </svg>
+    ),
     title: "SOLANA",
   },
 ];
@@ -205,6 +237,8 @@ export default function HomePage() {
           <p
             style={{
               marginTop: -23,
+
+              fontWeight: "normal",
             }}
           >
             {item.title}
@@ -261,8 +295,9 @@ export default function HomePage() {
               <p
                 style={{
                   fontFamily: "Montserrat",
+
                   fontStyle: "normal",
-                  fontWeight: "600",
+                  fontWeight: "500",
                   fontSize: 14,
 
                   color: colorMode === "dark" ? "white" : "#000D35",
@@ -290,28 +325,26 @@ export default function HomePage() {
       </div>
     );
   };
-
   const _renderSupportedBlockchains = () => {
     let rows = [];
-
     rows = supportedBlockchainsList.map((item, index) => {
       return (
         <div
           style={{
             display: "flex",
             flexDirection: "column",
-            justifyContent: "center",
             alignItems: "center",
+            width: "5%",
+            margin: "0 10px",
           }}
         >
           {item.logo}
           <p
             style={{
-              marginRight: 40,
-              marginTop: 15,
+              marginTop: index === 9 ? 20 : 15,
               fontFamily: "Montserrat",
               fontStyle: "normal",
-              fontWeight: "500",
+              fontWeight: "600",
               fontSize: 9,
               letterSpacing: 0.5,
               color: colorMode === "dark" ? "#FFFFFF" : "#000000",
@@ -325,15 +358,52 @@ export default function HomePage() {
 
     return (
       <div
+        className="container-fluid"
         style={{
+          width: "100%",
+          background: "rgba(255, 255, 255, 0.14)",
+          border: "1px solid #000D35",
+          borderRadius: 28,
+          marginTop: 30,
+          padding: 30,
+          paddingLeft: 50,
+          boxShadow: colorMode === "dark" ? null : "5px 5px black",
           display: "flex",
-          flexWrap: "wrap",
+          flexDirection: "column",
+          marginBottom: 60,
+          justifyContent: "space-evenly",
+          //alignItems: "center",
         }}
       >
-        {rows}
+        <div>
+          <div className="col-sm-12">
+            <p
+              style={{
+                fontSize: 13,
+                marginBottom: 5,
+              }}
+            >
+              {" "}
+              <br></br>
+              <b>SUPPORTED BLOCKCHAINS</b>
+              <br></br>
+              <br></br>
+            </p>
+            <div
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "space-evenly",
+              }}
+            >
+              {rows}
+            </div>
+          </div>
+        </div>
       </div>
     );
   };
+
   return (
     <div
       style={{
@@ -356,6 +426,7 @@ export default function HomePage() {
               fontWeight: "bold",
               marginBottom: 10,
               lineHeight: 1,
+              marginTop: 160,
             }}
           >
             <b>Web3 Wallet API & SDK</b>
@@ -366,16 +437,14 @@ export default function HomePage() {
           </p>
           <a
             href="/overview/whatIsWalletApiSdk"
-            type="button"
             style={{
               borderRadius: 22,
               background: "#9D4DFA",
-              fontWeight: "600",
               width: 150,
               padding: 15,
               color: "white",
-              textDecoration: "none",
             }}
+            class="button"
           >
             Learn more
           </a>
@@ -393,48 +462,14 @@ export default function HomePage() {
           }}
         </BrowserOnly> */}
 
-        <div className="d-none d-sm-block">
+        <div className="">
           {colorMode === "dark" ? <IntroLogoDark /> : <IntroLogoLight />}
         </div>
       </div>
-
       <h1 style={{ marginBottom: 50, marginTop: 20 }}>Wallet SDK</h1>
-
       {_renderWalletSdkNavigationRow()}
+      {_renderSupportedBlockchains()}
 
-      <div
-        className="container-fluid"
-        style={{
-          width: "100%",
-          background: "rgba(255, 255, 255, 0.14)",
-          border: "1px solid #000D35",
-          borderRadius: 28,
-          marginTop: 30,
-          padding: 30,
-          boxShadow: colorMode === "dark" ? null : "5px 5px black",
-          display: "flex",
-          flexDirection: "column",
-          marginBottom: 60,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <div className="row">
-          <div className="col-sm-12">
-            <p
-              style={{
-                fontSize: 13,
-              }}
-            >
-              {" "}
-              <b>SUPPORTED BLOCKCHAINS</b>
-              <br></br>
-              <br></br>
-              {_renderSupportedBlockchains()}
-            </p>
-          </div>
-        </div>
-      </div>
       <h1 style={{ marginBottom: 50 }}>Wallet APIs</h1>
       {_renderWalletAPI()}
     </div>

d8ab1cff96864337052cff3b01969407ed538b24
Johanna Fransson <adrina0002@gmail.com>
2023-04-04 16:30:34 +0100

    dont respect prefered system color scheme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -146,7 +146,7 @@ const config = {
       colorMode: {
         defaultMode: "dark",
         disableSwitch: false,
-        respectPrefersColorScheme: true,
+        respectPrefersColorScheme: false,
       },
     }),
 };
